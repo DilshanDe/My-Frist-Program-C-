6429c6433a3a3b969a07df40fe99718e1e8420fe
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-06-14 20:51:07 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
